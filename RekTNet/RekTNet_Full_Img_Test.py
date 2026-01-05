da711b8fa847c814608ec0e745017ea097ca67c0
ParthhhSoniii <parthhhsoniii@gmail.com>
2026-01-06 00:52:26 +0530

    Reset RekTNet tracking

--- RekTNet/RekTNet_Full_Img_Test.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("runs/pose/train/weights/best.pt")
-model.predict(
-    source="RekTNet_Full_Test_Imgs",  # directory of images
-    imgsz=128,
-    save=True,
-    conf=0.25
-)
